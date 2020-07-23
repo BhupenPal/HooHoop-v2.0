f179a6cdb45f2a51809182ea47ba028b4dc2363e
Varun Dev Sharma <dudevarun7777@gmail.com>
2020-07-23 10:38:13 +0530

    local changes

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
   mode: 'development',
   devServer: {
-    host: '192.168.1.7',
+    host: '192.168.0.114',
     port: 8080,
     disableHostCheck: true,
     historyApiFallback: true,
@@ -13,7 +13,7 @@ module.exports = {
     hot: true,
     proxy: {
       '/api': {
-        target: '192.168.1.7:3000',
+        target: '192.168.0.114:3000',
         changeOrigin: true
       }
     }
