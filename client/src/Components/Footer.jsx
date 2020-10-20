a73618ca17ac2d5bb3f62f6f595e5a635dd1357c
Bhupen Pal <bhupen16pal@gmail.com>
2020-10-20 11:30:16 +0530

    Footer component converted to a Functional Component

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,48 +1,62 @@
-import React, { Component } from "react"
-import { withRouter } from "react-router"
+// Dependency
+import React, { Fragment } from "react"
+import { withRouter, Link } from "react-router"
+
+// Styling
 import "../assets/css/footer.scss"
+
+// Images
 import Facebook from "../assets/img/svgs/facebook.svg"
 import Twitter from "../assets/img/svgs/twitter.svg"
 import Instagram from "../assets/img/svgs/instagram.svg"
-import { Link } from "react-router-dom"
 
-class Footer extends Component {
-  render() {
-    if (this.props.location.pathname === '/login' || this.props.location.pathname.includes("/register") || this.props.location.pathname.includes("/user/")) {
-      return null
-    }
-    return (
-      <footer className="footer">
-        
-        <div className="footer__follow-us">
-          <p>Follow us on</p>
-          <span className="footer__logo">
-            <img src={Facebook} />
-          </span>
-          <span className="footer__logo">
-            <img src={Twitter} />
-          </span>
-          <span className="footer__logo">
-            <img src={Instagram} />
-          </span>
-        </div>
-        <div>
-          <Link to="/about-us">About Us</Link>
-        </div>
-        <div>
-          <Link to="/faq">FAQ</Link>
-        </div>
-        <div>
-          <Link to="/privacy-policy">Privacy Policy</Link>
-        </div>
-        <div>
-          <Link to="/terms-and-conditions"> Terms of Use </Link>
-        </div>
-        <p className="footer__copyrights">Copyright &copy; HoopHoop.co.nz</p>
+const Footer = () => {
 
-      </footer>
-    )
+  const DoNotRenderFooter = () => {
+    return window.location.pathname === '/login' || window.location.pathname.includes("/register") || window.location.pathname.includes("/user/")
   }
+
+  return (
+    <Fragment>
+      {
+        (DoNotRenderFooter())
+          ?
+          null
+          :
+          (
+            <footer className="footer">
+
+              <div className="footer__follow-us">
+                <p>Follow us on</p>
+                <span className="footer__logo">
+                  <img src={Facebook} />
+                </span>
+                <span className="footer__logo">
+                  <img src={Twitter} />
+                </span>
+                <span className="footer__logo">
+                  <img src={Instagram} />
+                </span>
+              </div>
+              <div>
+                <Link to="/about-us">About Us</Link>
+              </div>
+              <div>
+                <Link to="/faq">FAQ</Link>
+              </div>
+              <div>
+                <Link to="/privacy-policy">Privacy Policy</Link>
+              </div>
+              <div>
+                <Link to="/terms-and-conditions"> Terms of Use </Link>
+              </div>
+              <p className="footer__copyrights">Copyright &copy; HoopHoop.co.nz</p>
+
+            </footer>
+          )
+      }
+    </Fragment>
+  )
 }
 
 export default withRouter(Footer)
