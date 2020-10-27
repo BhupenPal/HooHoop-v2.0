77e33b9d68d822d1dfb129e5d85a210e8b65ac9c
roopamgarg <roopamg777@gmail.com>
2020-10-28 01:03:41 +0530

    adding media queries

--- client/src/axios.js
+++ client/src/axios.js
@@ -6,19 +6,20 @@ import jwt_decode from "jwt-decode";
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
   // Do something before request is sent
-  console.log("hi")
-
+ //   console.log("hi")
+ // console.log(config)
   //console.log(config,store.getState().auth?.token)
   // if(error.response.status === 401){
   //   store.dispatch(refreshUserToken());
   //  }
  // const state = store.getState();
 
-  const user = jwt_decode(localStorage.getItem("accessToken"));
-  console.log()
+  //const user = jwt_decode(localStorage.getItem("accessToken"));
+ // console.log()
   // if(accessToken){
   //   const decoded = jwt_decode
   // }
+  console.log(config)
   return config;
 }, function (error) {
   // Do something with request error
@@ -38,15 +39,13 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(
   function (response) {
     console.log(store.getState())
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return response;
   },
   function (error) {
-    // Any status codes that falls outside the range of 4xx cause this function to trigger
-    // Do something with response error
-   // console.log(error.message,error.response.status);
-   
+    console.log(error.message,error.response.status);
+    if(error.response && error.response.status === 401){
+       store.dispatch(refreshUserToken());
+    }
     return Promise.reject(error);
   }
 )
