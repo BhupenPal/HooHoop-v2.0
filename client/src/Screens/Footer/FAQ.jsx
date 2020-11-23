9945ac9a90d379fd4e5769e6bf42bdae6e10215c
Bhupen Pal <bhupen16pal@gmail.com>
2020-11-24 00:07:08 +0530

    Content added to FAQ page

--- client/src/Screens/Footer/FAQ.jsx
+++ client/src/Screens/Footer/FAQ.jsx
@@ -29,8 +29,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            In a nutshell, Hoohoop is an online marketplace purpose designed for Kiwi’s to make the selling and buying process of cars more simplistic and transparent, therefore giving you the confidence you deserve with your asset and investment!
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -45,8 +44,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            Our passion for creating an honest platform with 100% transparency to efficiently buy and sell vehicles is what differentiates us from others. By providing our 360 virtual tour, you are able to get a great feel for the vehicle. The clarity and different angles you can access allows you to check for any internal or external damage, there is nowhere to hide! 
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -61,8 +59,14 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                        Easily done! Simply head along to the ‘Sell Your Car’ section of our website and complete the following steps below: 
+                        <ul>
+                          <li>Enter your vehicle number plate</li>
+                          <li>Fill in all the details regarding your vehicle from our user friendly form.</li>
+                          <li>At the bottom of the form, request a 360 virtual service</li>
+                          <li>Your car will be listed once the virtual experience has been created.</li>
+                        </ul>
+                        <span>NB: There is a small fee associated with the listing of your vehicle.</span>
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -77,8 +81,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            Yes and there is a couple of main reasons for this. We only list vehicles that have this completed because we are about promoting authenticity and confidence to the potential buyer, but this also increases the chance of a purchase for the seller. It is important that the customer gets to have a ‘real life’ experience and that the seller gets their vehicle promoted to a high standard. 
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -93,8 +96,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            Our passion for creating an honest platform with 100% transparency to efficiently buy and sell vehicles is what differentiates us from others. By providing our 360 virtual tour, you are able to get a great feel for the vehicle. The clarity and different angles you can access allows you to check for any internal or external damage, there is nowhere to hide!
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -109,8 +111,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            There are no restrictions regarding listing any sort of cars, in which are available in New Zealand. You can even list: Hatchback, Sedan, MPV, SUV, Crossover, Coupe, and even convertibles on the website. 
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -125,8 +126,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            You can pay online using any credit/debit, visa or master-card. Internet banking is also available if this suits your needs. We do not save any information about your card; and all the information processed is fully encrypted.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -141,8 +141,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            We request and ask the seller when they are going through the process of uploading the details to provide a holistic overview of what the buyer would be looking to see, so when complete these details are available in the public domain. If you need additional details; you can make a query from the car description page directly to the car owner to seek clarification if required.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -157,8 +156,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            Why not? Seeing is believing! We encourage you to take a test drive by requesting for a test drive directly from the website. You can determine a convenient schedule that works for both parties to take the car for a spin!
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -173,8 +171,7 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            For refund policy please visit <a href="/privacy-policy">Privacy Policy</a>.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -189,8 +186,11 @@ const FAQ = (props) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
+                            You can call us on <a href="tel:021-114-3347">(021) 114-3347</a> or email us at <a href="mailto:info@hoohoop.co.nz" target="__blank">info@hoohoop.co.nz</a>
+                            <br/>
+                            Alternatively, you can contact us through private message on our HooHoop Facebook Page.
+                            <br/>
+                            or, just simply send us your query through our <a href="/contact-us">Contact Us</a> page.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
