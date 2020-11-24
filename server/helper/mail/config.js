b806005707f25b3c37282998a293284c48374ec1
Bhupen Pal <bhupen16pal@gmail.com>
2020-11-24 23:50:26 +0530

    Nodmailer changed with Send Grid

--- server/helper/mail/config.js
+++ server/helper/mail/config.js
@@ -1,24 +1,50 @@
-const nodemailer = require("nodemailer"),
-    transporter = nodemailer.createTransport({
-    host: 'cp-wc12.lon01.ds.network',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'contact@hoohoop.co.nz',
-        pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-});
+const axios = require('axios')
+const RunPlatform = process.env.NODE_ENV === 'DEV' ? `http://${process.env.HOST_IP}:${process.env.CLIENT_PORT}` : 'https://www.hoohoop.co.nz'
 
 module.exports = {
-    SendMail: (ToEmail, MailSubject, MailHTML) => {
-        transporter.sendMail({
-            from: '"HooHoop" <contact@hoohoop.co.nz>',
-            to: ToEmail,
-            subject: MailSubject,
-            html: MailHTML
+    SendMail: (ToEmail, MailSubject, MailContent) => {
+        const transporter = {
+            personalizations: [
+                {
+                    to: [{
+                        email: ToEmail
+                    }],
+                    dynamic_template_data: {
+                        ...MailContent.Data,
+                        RunPlatform
+                    },
+                    subject: MailSubject
+                }
+            ],
+            from: {
+                email: 'contact@hoohoop.co.nz',
+                name: 'HooHoop Contact'
+            },
+            reply_to: {
+                email: 'contact@hoohoop.co.nz',
+                name: 'HooHoop Contact'
+            },
+            template_id: MailContent.template_id
+        }
+
+        if (process.env.NODE_ENV === 'DEV') {
+            transporter.mail_settings = {
+                sandbox_mode: {
+                    enable: true
+                }
+            }
+        }
+
+        axios({
+            method: 'POST',
+            url: 'https://api.sendgrid.com/v3/mail/send',
+            data: transporter,
+            withCredentials: true,
+            headers: {
+                'Authorization': `Bearer ${process.env.SENDGRID_API_KEY}`
+            }
         })
+            .then()
+            .catch(error => console.log(error.message))
     }
 }
