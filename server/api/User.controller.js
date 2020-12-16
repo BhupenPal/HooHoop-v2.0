0a09c06bbe9b5e58b3996eb0f1bd04bd3c039d00
Bhupen Pal <bhupen16pal@gmail.com>
2020-12-17 01:19:59 +0530

    User Details Added in Refresh Token

--- server/api/User.controller.js
+++ server/api/User.controller.js
@@ -51,10 +51,8 @@ Router.post("/login", async (req, res, next) => {
     if (!User.EmailVerified)
       throw createError.BadRequest("Please activate your account.")
 
-    if (!User.isActive)
-      throw createError.BadRequest(
-        "Your account is temporarily deactivated. Please contact HooHoop NZ"
-      )
+    if (!User.isActive) 
+      throw createError.BadRequest("Your account is temporarily deactivated. Please contact HooHoop NZ")
 
     bcrypt.compare(Password, User.Password, async (err, isMatch) => {
       if (!isMatch) return next(createError.Forbidden("Password does not match"))
